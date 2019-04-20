063a26fce9de4362c09117a71f7831cdb54163a9
Sebastian Moreno R <sebastian.mr47@gmail.com>
2019-04-20 01:54:15 -0500

    Data to CSV

---
+++ toCSV.py
@@ -0,0 +1,30 @@
+from settings import PROJECT_ROOT
+import os
+
+def main():
+    print(PROJECT_ROOT + "\\DataCSV")
+    path = PROJECT_ROOT + "\\DataCSV"
+    pathCSV = PROJECT_ROOT + "\\DataCSV\\fileDataCSV.txt"
+    files = []
+    # r=root, d=directories, f = files
+    for r, d, f in os.walk(path):
+        for file in f:
+            if 'Metrics.txt' in file:
+                files.append(os.path.join(r, file))
+    first = True
+    csvFile = open(pathCSV,"w+")
+    for f in files:
+        file = open( f , "r")
+        if ( first ):
+            csvFile.write( ";".join( file.readline().strip().split(",") ) + "\n" )
+            csvFile.write( ";".join( file.readline().strip().split(",") ) + "\n" )
+            first = False
+        else:
+            file.readline()
+            csvFile.write( ";".join( file.readline().strip().split(",") ) + "\n" )
+        print(f)
+        file.close()
+    csvFile.close()
+
+if __name__ == '__main__':
+    main()
